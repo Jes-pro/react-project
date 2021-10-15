b231a6dd76ba2a48d90de1617a48b2e84f641b33
shopilyv@gmail.com <shopilyv@gmail.com>
2021-10-15 17:54:43 +0300

    added card and functions

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React from "react";
+import Todo from "./components/Todo";
 
 function App() {
   return (
-    <div>Hello</div>
+    <div>
+      <h1>My Todos</h1>
+      <Todo text="first todo" />
+      <Todo text="second todo" />
+      <Todo text="third todo" />
+    </div>
   );
 }
 
